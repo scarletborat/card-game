f551b7bf0e5c8a4678f979d57f922c469b19b93a
Scarlet <scarletborat@ukr.net>
2023-05-29 13:54:43 -0700

    Separate tests by functionality

---
+++ test-game_circle.py
@@ -0,0 +1,43 @@
+import unittest
+from unittest import TestCase
+from unittest.mock import patch
+from io import StringIO
+import sys
+
+from card import Card
+from game import Game
+
+class Stub(Game):
+  def invite_players(self):
+      return ['John', 'Jane']
+  
+  def small_circle(self, attackers, attacker_index, defender):
+    attacker = attackers[attacker_index]
+    attacker_cards = [attacker.place_card(f"{card.get_suit()}:{card.get_value()}") for card in attacker.get_cards()]
+    defender.draw_cards(attacker_cards)
+    return 1
+      
+class GameCircle(TestCase):
+    def test_user_lost(self):
+      game = Stub()
+      game.deck.deck = []
+
+      player_cards = [
+        [Card(suit='heart', value=6), Card(suit='heart', value=7)],
+        [Card(suit='diamond', value=6), Card(suit='diamond', value=7), Card(suit='diamond', value=8)]
+      ]
+
+      for i, player in enumerate(game.players):
+         player.cards = player_cards[i]
+
+      captured_output = StringIO()
+      sys.stdout = captured_output
+
+      game.game_circle()
+      actual_output = captured_output.getvalue()
+      sys.stdout = sys.__stdout__
+
+      self.assertEqual(actual_output, f"Game over! {game.players[1].get_name().title()} you lost\n")
+
+if __name__ == '__main__':
+    unittest.main()
--- test-game_invite-players.py
+++ test-game_invite-players.py
@@ -6,15 +6,15 @@ import sys
 
 from game import Game
 
-class TestGame(TestCase):
+class InvitePlayers(TestCase):
     @patch('builtins.input', side_effect=['John', 'Jane', 'start'])
-    def test_invite_players_positive_case(self, mock_input):
+    def test_positive_case(self, mock_input):
         game = Game()
         players = game.invite_players()
         self.assertEqual(players, ['John', 'Jane'])
 
     @patch('builtins.input', side_effect=['John', 'start', 'q'])
-    def test_invite_players_two_user_min(self, mock_input):
+    def test_two_user_min(self, mock_input):
         game = Game()
         expected_output = "Must be at least 2 players in the game\n"
 
@@ -29,11 +29,13 @@ class TestGame(TestCase):
         self.assertEqual(actual_output, expected_output)
 
     @patch('builtins.input', side_effect=['John', 'Anton', 'q'])
-    def test_invite_players_quit(self, mock_input):
+    def test_quit(self, mock_input):
         game = Game()
         players = game.invite_players()
         print(f"Players: {players}")
         self.assertEqual(players, None)
+
+    
 
 if __name__ == '__main__':
     unittest.main()
